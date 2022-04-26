79984a89cece813f2f2160256e71327cdf389c6d
nascilino <nascilino@gmail.com>
2022-04-26 08:44:17 -0300

    main

---
+++ src/middlewares/characters.middlaware.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const validId = (req, res, next) => {
+  const idParam = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    return res.status(400).send({ message: "Id inválido!" });
+  }
+  next();
+};
+
+const validObjectBody = (req, res, next) => {
+  const people = req.body;
+  if (!people.nome || !people.descricao || !people.preco) {
+    return res
+      .status(400)
+      .send({ message: "Envie o todos os campos!" });
+  }
+  next();
+};
+
+module.exports = {
+  validId,
+  validObjectBody,
+};
