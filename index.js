1b604fd3de2299332783dc339a570871a686828e
nascilino <nascilino@gmail.com>
2022-05-09 18:39:15 -0300

    correção login

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectToDatabase = require("./src/database/database");
 const routes = require("./src/routes/characters.routes");
 const userRoutes = require("./src/users/users.routes");
 const authRoutes = require("./src/auth/auth.routes");
-const connectToDatabase = require("./src/database/database");
+
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 connectToDatabase();
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
-app.use(express.json());
-app.use(cors());
+
+
 app.use(routes);
 app.use(userRoutes);
 app.use(authRoutes);
